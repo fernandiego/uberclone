7447fce13cb4b0e58bf49163b240e5e90836dec3
fox <fernandodiego.sn@gmail.com>
2023-11-27 15:33:56 -0300

    Showing the first boxes and icons

--- uber/src/views/HomeView.vue
+++ uber/src/views/HomeView.vue
@@ -1,8 +1,17 @@
 <template>
   <div id="Home" class="flex flex-col h-screen">
     <div class="m-3 mb-auto">
-      <div class="grid grid-cols2 gap-3 my-3">
-      <ServiceSelectLarge></ServiceSelectLarge>
+      <div class="grid grid-cols-2 gap-3 my-3">
+      <ServiceSelectLarge
+        text="Ride"
+        imageWidth="74"
+        image="ride"
+      />
+      <ServiceSelectLarge
+        text="Food"
+        imageWidth="74"
+        image="food"
+      />
       </div>
     </div>
   </div>
