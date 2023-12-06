ee1695304d16c96836c5dbc08476f835c1d3dca9
fox <fernandodiego.sn@gmail.com>
2023-12-06 18:03:17 -0300

    index.js server, preparing the server

---
+++ uber/server/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const axios = require('axios')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+const port = 4001
