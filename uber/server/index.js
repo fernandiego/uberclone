7f5181591fde13603f3a81f14eba89771996aec8
fox <fernandodiego.sn@gmail.com>
2023-12-06 18:50:48 -0300

    index.js server, preparing the server

--- uber/server/index.js
+++ uber/server/index.js
@@ -5,3 +5,33 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 require('dotenv').config()
 const port = 4001
+
+axios.default.baseURL = process.env.GOOGLE_MAPS_API_URL
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/api/address/:address', async (req, res) => {
+try {
+    if (
+        req.params.address === null ||
+        req.params.address === '' ||
+        req.params.address === 'null'
+    ) return ''
+    let data = axios.get(
+        'place/autocomplete/json' +
+        '?input=' + req.params.address +
+        '&types=address' +
+        '&key=' + process.env.GOOGLE_MAPS_API_KEY
+    )
+
+    res.status(200).json(data.data.predictions)
+} catch (err) {
+    console.log(err)
+}
+})
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+})
+
