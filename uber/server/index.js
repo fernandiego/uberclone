504f94f6a4ab5c9e26c020560ea845f5db810585
fox <fernandodiego.sn@gmail.com>
2023-12-07 18:20:25 -0300

    having problems with the google maps api

--- uber/server/index.js
+++ uber/server/index.js
@@ -4,7 +4,7 @@ const axios = require('axios')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 require('dotenv').config()
-const port = 4001
+const port = process.env.PORT || 4001
 
 axios.defaults.baseURL = process.env.GOOGLE_MAPS_API_KEY
 
@@ -12,24 +12,25 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.get('/api/address/:address', async (req, res) => {
-try {
-    if (
-        req.params.address === null ||
-        req.params.address === '' ||
-        req.params.address === 'null'
-    ) return ''
-    let data = await axios.get(
-        'place/autocomplete/json' +
-        '?input=' + req.params.address +
-        '&types=address' +
-        '&key=' + process.env.VUE_APP_GOOGLE_MAPS_API_KEY
-    )
+    try {
+        if (!req.params.address) {
+            return res.status(400).json('Address parameter is missing');
+        }
 
-    res.status(200).json(data.data.predictions)
-} catch (err) {
-    console.log(err)
-}
-})
+        let data = await axios.get(
+            'place/autocomplete/json' +
+            '?input=' + encodeURIComponent(req.params.address) + // Make sure to encode the address
+            '&types=address' +
+            '&key=' + process.env.VUE_APP_GOOGLE_MAPS_API_KEY
+        );
+
+        res.status(200).json(data.data.predictions);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json('Internal Server Error');
+    }
+});
+
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
